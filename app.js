365d5b68f78d32993d741dbc9d32aad55de36a54
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-10-13 17:25:07 -0400

    ng-show ng-model

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 angular.module('myApp', [require('angular-animate')])
+	.controller('myCtrl', function(){
+		var main = this;
+		main.tag = "";
+		main.submitted = false;
+
+		main.submitForm = function(valid){
+			main.submitted = true;
+		}
+
+		main.reset = function(){
+			main.submitted = false;
+			main.tag = "";
+		};
+	});
