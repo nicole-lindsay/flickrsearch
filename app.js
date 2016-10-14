e5d60a88edddb47add595912605e89dbf42b2bc6
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-10-14 14:31:20 -0400

    trying to get to different pages

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ angular.module('myApp', ['ngAnimate'])
     .controller('myCtrl', function($http) {
         var main = this;
         main.tag = "";
+        main.placeholder = "";
         main.valid = false;
         main.error = false;
         main.foundPhotos = false;
         main.results = [];
+        main.results.page = 1;
 
         main.getPhoto = function() {
             var url = "https://api.flickr.com/services/rest";
@@ -14,34 +16,48 @@ angular.module('myApp', ['ngAnimate'])
                 api_key: '86ea026a9ba3960e6eb98657b7ea5cb9',
                 tags: main.tag,
                 format: 'json',
+                page: main.results.page,
                 nojsoncallback: 1
             };
 
             $http({
-            	method: 'GET',
-            	url: url,
-            	params: params
-            })
-            .then(function(response){
-            	main.foundPhotos = true;
-            	main.results = response.data.photos;
-            	main.error = false;
-            	// console.log(main.results); //works here
-            },
-            function(response){
-            	main.error = true;
-            });
+                    method: 'GET',
+                    url: url,
+                    params: params
+                })
+                .then(function(response) {
+                        main.foundPhotos = true;
+                        main.results = response.data.photos;
+                        main.error = false;
+                        var placeholder = main.tag;
+                        main.tag = "";
+                        main.placeholder = placeholder;
+                    },
+                    function(response) {
+                        main.error = true;
+                    });
         };
 
-        main.searchPhoto = function(word){
-        	if(word){
-        		main.valid = true;
-        	} else {
-        		main.valid = false;
-        		return;
-        	}
-        	main.word = word;
-        	main.getPhoto();
+        main.searchPhoto = function(word) {
+            if (word) {
+                main.valid = true;
+            } else {
+                main.valid = false;
+                return;
+            }
+            main.word = word;
+            main.results.page = 1;
+            main.getPhoto();
         };
 
+        main.back = function() {
+            main.results.page -= 1;
+            main.searchPhoto();
+        };
+
+        main.forward = function() {
+            main.results.page += 1;
+            main.searchPhoto();
+            console.log(main.results);
+        };
     });
