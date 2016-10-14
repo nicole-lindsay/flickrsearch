5a70c51cf6c448795f6f16b236694bb36dfd4586
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-10-13 20:01:08 -0400

    I seriously forgot to add angular -.-

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-angular.module('myApp', [require('angular-animate')])
+angular.module('myApp', ['ngAnimate'])
 	.controller('myCtrl', function(){
 		var main = this;
 		main.tag = "";
 		main.submitted = false;
+		main.searchMsg = false;
+		main.imgMsg = "";
+		main.apiMsg = "";
 
-		main.submitForm = function(valid){
+		main.submitForm = function(tag){
+			console.log("something");
 			main.submitted = true;
+			main.searchMsg = true;
+			main.tag = "";
 		}
 
-		main.reset = function(){
-			main.submitted = false;
-			main.tag = "";
-		};
 	});
